858aa46a45cfebbd9ff1898b3f73c43cd76da3e5
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-05-13 09:35:26 +0100

    Iterating...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,50 @@
 <template>
-  <div class="w-full p-14 text-white">
-    <section class="flex justify-between">
-<router-link :to="{path: '/'}"><div class="flex items-center justify-center">
-<img src="../assets/logo.png" alt="currency-conversion-logo" class="w-12 h-12 rounded-full">
-<p class="uppercase font-bold ml-3 ">currency-conversion</p>
-</div></router-link>
-<div class="flex justify-center capitalize space-x-7">
-<p class="text-lg"><router-link :to="{path: '/learn'}">learn</router-link></p>
-<p class="text-lg"><router-link :to="{path: '/developers'}">developers</router-link></p>
-<p class="text-lg"><router-link :to="{path: '/enterprise'}">enterprise</router-link></p>
-<p class="text-lg"><router-link :to="{path: '/community'}">community</router-link></p>
-</div>
+  <div class="w-full md:p-20 p-12">
+    <section class="">
+      <div class="flex items-center justify-between text-gray-300">
+        <router-link :to="{ path: '/' }">
+          <div class="flex justify-center items-center">
+            <img
+              src="../assets/logo.png"
+              alt="currency-conversion-logo"
+              class="w-12 h-12 rounded-full"
+            />
+            <p class="uppercase font-bold ml-4 md:text-base text-sm">
+              currency-converter
+            </p>
+          </div></router-link
+        >
+        <div
+          class="flex justify-center capitalize md:space-x-14 space-x-2 lg:mr-64 font-semibold md:text-base text-xs"
+        >
+          <p class=""><router-link :to="{ path: '/learn' }">learn</router-link></p>
+          <p class="">
+            <router-link :to="{ path: '/developers' }">developers</router-link>
+          </p>
+          <p class="">
+            <router-link :to="{ path: '/enterprise' }">enterprise</router-link>
+          </p>
+          <p class="">
+            <router-link :to="{ path: '/community' }">community</router-link>
+          </p>
+        </div>
+      </div>
+      <div class="flex justify-between mt-16">
+        <div class="space-y-14">
+          <p class="text-white text-7xl font-semibold">
+            Created for easy currency-conversion.<br />
+            World-class currency conversion tool.
+          </p>
+          <button
+            class="bg-white text-base font-semibold capitalize rounded-lg py-3 px-4"
+          >
+            explore currency-converter
+          </button>
+        </div>
+        <div>
+          <img src="../assets/logo.png" alt="" />
+        </div>
+      </div>
     </section>
   </div>
 </template>

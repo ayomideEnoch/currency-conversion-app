165a0eb1fde5dd3559606fc847b3424b397f1e33
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-05-13 08:35:31 +0100

    Iterating...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="w-full p-14">
+  <div class="w-full p-14 text-white">
     <section class="flex justify-between">
 <router-link :to="{path: '/'}"><div class="flex items-center justify-center">
 <img src="../assets/logo.png" alt="currency-conversion-logo" class="w-12 h-12 rounded-full">
-<p class="uppercase text-white font-bold ">currency-conversion</p>
+<p class="uppercase font-bold ml-3 ">currency-conversion</p>
 </div></router-link>
-<div class="flex justify-center capitalize">
-<p><router-link :to="{path: '/learn'}">learn</router-link></p>
-<p><router-link :to="{path: '/developers'}">developers</router-link></p>
-<p><router-link :to="{path: '/enterprise'}">enterprise</router-link></p>
-<p><router-link :to="{path: '/community'}">community</router-link></p>
+<div class="flex justify-center capitalize space-x-7">
+<p class="text-lg"><router-link :to="{path: '/learn'}">learn</router-link></p>
+<p class="text-lg"><router-link :to="{path: '/developers'}">developers</router-link></p>
+<p class="text-lg"><router-link :to="{path: '/enterprise'}">enterprise</router-link></p>
+<p class="text-lg"><router-link :to="{path: '/community'}">community</router-link></p>
 </div>
     </section>
   </div>

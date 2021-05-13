6fa5ff7c6953a3488418b6d928b3ec36dbd2bcc0
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-05-13 08:27:11 +0100

    Iterating...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full p-14">
     <section class="flex justify-between">
-<div class="">
-<img src="../assets/logo.png" alt="currency-conversion-logo">
-<p class="uppercase">currency-converter</p>
-</div>
+<router-link :to="{path: '/'}"><div class="flex items-center justify-center">
+<img src="../assets/logo.png" alt="currency-conversion-logo" class="w-12 h-12 rounded-full">
+<p class="uppercase text-white font-bold ">currency-conversion</p>
+</div></router-link>
 <div class="flex justify-center capitalize">
-<p><router-link :to="{path: '/'}">learn</router-link></p>
+<p><router-link :to="{path: '/learn'}">learn</router-link></p>
 <p><router-link :to="{path: '/developers'}">developers</router-link></p>
 <p><router-link :to="{path: '/enterprise'}">enterprise</router-link></p>
 <p><router-link :to="{path: '/community'}">community</router-link></p>

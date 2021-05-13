0423f01ab0ec9fc513170fa086ec361d14437129
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-05-13 08:16:37 +0100

    Iterating...

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 import Learn from '../views/Learn.vue'
 import Developers from '../views/Developers.vue'
 import Enterprise from '../views/Enterprise.vue'
@@ -11,6 +12,11 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/learn',
     name: 'Learn',
     component: Learn
   },
--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -1,17 +1,6 @@
 <template>
-  <div class="w-full p-14">
-    <section class="flex justify-between">
-<div class="">
-<img src="../assets/logo.png" alt="currency-conversion-logo">
-<p class="uppercase">currency-converter</p>
-</div>
-<div class="flex justify-center capitalize">
-<p><router-link :to="{path: '/'}">learn</router-link></p>
-<p><router-link :to="{path: '/developers'}">developers</router-link></p>
-<p><router-link :to="{path: '/enterprise'}">enterprise</router-link></p>
-<p><router-link :to="{path: '/community'}">community</router-link></p>
-</div>
-    </section>
+  <div class="w-full">
+    <div class="">Learn</div>
   </div>
 </template>
 
